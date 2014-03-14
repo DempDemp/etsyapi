c1bdfa6ae582127740b9e8ce59cdb08322f9b3c1
DempDemp <lidor@lidor.net>
2014-03-14 11:32:01 +0200

    Version upgrade

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='etsyapi',
-    version='0.2',
+    version='0.2.1',
     description='etsy.com api wrapper',
     url='https://github.com/DempDemp/etsyapi',
-    download_url = 'https://github.com/DempDemp/etsyapi/tarball/0.2',
+    download_url = 'https://github.com/DempDemp/etsyapi/tarball/0.2.1',
     packages=['etsyapi'],
     keywords=['etsy', 'api', 'etsy.com'],
     license='BSD',
