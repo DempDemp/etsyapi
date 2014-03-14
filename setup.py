b5f7617f646ece74f5794f89ea696ab875a0a34b
DempDemp <lidor@lidor.net>
2014-03-14 10:57:12 +0200

    Setup changes

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
     name='etsyapi',
     version='0.2',
-    packages=['etsyapi',],
+    description='etsy.com api wrapper',
+    url='https://github.com/DempDemp/etsyapi',
+    download_url = 'https://github.com/DempDemp/etsyapi/tarball/0.1',
+    packages=['etsyapi'],
+    keywords=['etsy', 'api', 'etsy.com'],
     license='BSD',
     long_description=open('README.md').read(),
     install_requires=[
